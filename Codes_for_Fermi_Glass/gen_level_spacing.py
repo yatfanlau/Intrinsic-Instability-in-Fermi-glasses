df1c6865e6582b036eced05561cd699e04c47beb
Yat Fan Lau <90203935+yatfanlau@users.noreply.github.com>
2024-08-29 21:45:54 +0800

    Update gen_level_spacing.py

--- Codes_for_Fermi_Glass/gen_level_spacing.py
+++ Codes_for_Fermi_Glass/gen_level_spacing.py
@@ -1,76 +1,62 @@
-from __future__ import print_function
-import math
-import random
-import sys
 import numpy as np
 from scipy import linalg
 import matplotlib.pyplot as plt
-from random import randint
 from scipy.optimize import curve_fit
 from scipy.special import factorial
 from scipy.stats import poisson
 import proplot
 
-
-L = 60 #int(input('System size: ')) 
-W = 9 #float(input('Disorder strength: '))
+# Constants
+L = 60
+W = 9
 Loc = 7.65151489
 tp = 0.6
+R = 0.5  # Radius of the grain
 
-def generate_disorder(L,W):
-  disorder=W*((np.random.uniform(size=L*L)).reshape((L,L))-0.5)
-  return disorder
-  
-def generate_hamiltonian(L,W):
-  H=np.zeros((L*L,L*L))
-  disorder=generate_disorder(L,W)
-  for i in range(L):
-    ip1=(i+1)%L
-    im1=(i-1)%L
-    for j in range(L):
-      H[i*L+j,i*L+j]=disorder[i,j]
-      jp1=(j+1)%L
-      jm1=(j-1)%L
-      H[ip1*L+j  ,i  *L+j  ]=1.0
-      H[i*  L+j  ,ip1*L+j  ]=1.0
-      H[i  *L+jp1,i  *L+j  ]=1.0
-      H[i*  L+j  ,i  *L+jp1]=1.0
-      
-      H[i*L+j,ip1*L+jp1] = tp
-      H[ip1*L+jp1,i*L+j] = tp
-      H[i*L+j,ip1*L+jm1] = tp
-      H[ip1*L+jm1,i*L+j] = tp
-      
-  return H
+def generate_disorder(L, W):
+    """Generate a disorder matrix for the Hamiltonian."""
+    disorder = W * (np.random.uniform(size=(L, L)) - 0.5)
+    return disorder
 
-def findmax(index):
-    eigenstate = []
-    for i in range(L*L):
-        eigenstate.append(eigenstates2[i,int(index)])
-    ep = np.argsort(eigenstate)
-    position = ep[L*L-1]+1
-    if position%L==0:
-        x,y = L,(position//L)
-    else:
-        x,y = position%L,(position//L)+1
-    return x,y
+def generate_hamiltonian(L, W):
+    """Generate the Hamiltonian matrix for a 2D lattice with periodic boundary conditions."""
+    H = np.zeros((L*L, L*L))
+    disorder = generate_disorder(L, W)
+    for i in range(L):
+        for j in range(L):
+            # Index mapping
+            index = i * L + j
+            H[index, index] = disorder[i, j]
+            neighbors = [(i, (j+1) % L), ((i+1) % L, j), (i, (j-1) % L), ((i-1) % L, j), 
+                         ((i+1) % L, (j+1) % L), ((i+1) % L, (j-1) % L)]
+            for ni, nj in neighbors:
+                neighbor_index = ni * L + nj
+                H[index, neighbor_index] = tp if ni != i and nj != j else 1.0
+    return H
 
-def d(x1,y1,x2,y2):
-    dist = np.sqrt((x1-x2)**2+(y1-y2)**2)/Loc
-    return dist
-R = 0.5  #radius of the grain
+def find_max(eigenstates, index, L):
+    """Find the position of maximum probability in the eigenstate."""
+    eigenstate = eigenstates[:, index]**2
+    max_index = np.argmax(eigenstate)
+    x, y = max_index % L, max_index // L + 1
+    return x + 1, y if x == L - 1 else y
+
+def distance(x1, y1, x2, y2):
+    """Calculate normalized distance."""
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2) / Loc
+
+# Main execution
 energy_spacing = []
 for con in range(30):
-    H=generate_hamiltonian(L,W)
-    (energy_levels,eigenstates)=linalg.eigh(H)
-    eigenstates2 = eigenstates**2
+    H = generate_hamiltonian(L, W)
+    energy_levels, eigenstates = linalg.eigh(H)
     energy = []
-    for s in range(L*L):
-        distance = d(30,30,findmax(s)[0],findmax(s)[1])
-        if distance<R:
+    for s in range(L * L):
+        x, y = find_max(eigenstates, s, L)
+        if distance(30, 30, x, y) < R:
             energy.append(energy_levels[s])
     energy.sort()
-    for k in range(len(energy)-1):
-        level_spacing = abs(energy[k]-energy[k+1])
-        energy_spacing.append(level_spacing)
-np.save("l="+str(2*R),energy_spacing)
+    energy_spacing.extend(np.diff(energy))
+
+# Saving the energy level spacings
+np.save(f"l={2 * R}", energy_spacing)
